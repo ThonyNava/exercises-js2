49f46ff6c88df7b4607e38a1ef7b40a375924f6f
Thony Nava <thonynava@Thonys-MBP.home>
2020-09-15 14:57:35 +0200

    Homework JS week-2 | week 8

--- week-2/Homework/1-exercises/exercises.js
+++ week-2/Homework/1-exercises/exercises.js
@@ -13,9 +13,48 @@
  *      .....
  * </div>
  */
+
+let user_1 = {
+  firstName: "Thony",
+  lastName: "Nava",
+  job: "Founder, CEO, & CTO",
+};
+
+let user_2 = {
+  firstName: "Gwynne",
+  lastName: "Shotwell",
+  job: "President & COO",
+};
+
+let user_3 = {
+  firstName: "Mark",
+  lastName: "Juncosa",
+  job: "VP, Vehicle Engineering",
+};
+
+let namesDB = [user_1, user_2, user_3];
+
 function exerciseOne(arrayOfPeople) {
   let content = document.querySelector("#content");
+  let div = document.createElement("div");
+  arrayOfPeople.forEach((obj) => {
+    let article = document.createElement("article");
+    article.className = "article";
+
+    let h1 = document.createElement("h1");
+    h1.innerHTML = obj.firstName + " " + obj.lastName;
+
+    let h2 = document.createElement("p");
+    h2.innerHTML = obj.job;
+
+    article.appendChild(h1);
+    article.appendChild(h2);
+    div.appendChild(article);
+  });
+  content.appendChild(div);
 }
+
+exerciseOne(namesDB);
 
 /**
  *
@@ -24,9 +63,47 @@ function exerciseOne(arrayOfPeople) {
  * All of your HTML should go inside the Div tag with the id "content".
  *
  */
+
+let product_1 = {
+  id: 1,
+  name: "Kaizen Cure Program",
+  price: 347,
+};
+
+let product_2 = {
+  id: 2,
+  name: "Being a professional SiHuber",
+  price: 780,
+};
+
+let product_3 = {
+  id: 3,
+  name: "Productivity 360° Program",
+  price: 997,
+};
+
+let products = [product_1, product_2, product_3];
+
 function exerciseTwo(shopping) {
-  //Write your code in here
+  let content = document.querySelector("#content");
+  let ul = document.createElement("ul");
+  shopping.forEach((obj) => {
+    let li = document.createElement("li");
+
+    let h1 = document.createElement("h1");
+    h1.innerHTML = obj.name;
+
+    let h2 = document.createElement("p");
+    h2.innerHTML = "€ " + obj.price;
+
+    li.appendChild(h1);
+    li.appendChild(h2);
+    ul.appendChild(li);
+  });
+  content.appendChild(ul);
 }
+
+exerciseTwo(products);
 
 /**
     I'd like to display my three favorite books inside a nice webpage!
@@ -57,8 +134,38 @@ function exerciseTwo(shopping) {
 
     The end result should look something like this: https://hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
 **/
-function exerciseThree(books) {
-  //Write your code in here
+
+function exerciseThree(booksArr) {
+  let content = document.querySelector("#content");
+  let ul = document.createElement("ul");
+  ul.style.display = "flex";
+  ul.style.listStyle = "none";
+  booksArr.forEach((obj) => {
+    let li = document.createElement("li");
+    li.style.margin = "15px";
+    li.style.padding = "10px";
+    li.style.minWidth = "350px";
+    li.style.borderRadius = "5px";
+    li.style.textAlign = "center";
+    li.style.width = "calc(25% - 51px)";
+
+    let p = document.createElement("p");
+    p.innerHTML = obj.title + " – " + obj.author;
+
+    let img = document.createElement("img");
+    img.src = obj.url;
+
+    if (obj.alreadyRead) {
+      li.style.backgroundColor = "rgba(50,205,50,.5)";
+    } else {
+      li.style.backgroundColor = "rgba(220,20,60,.5)";
+    }
+
+    li.appendChild(p);
+    li.appendChild(img);
+    ul.appendChild(li);
+  });
+  content.appendChild(ul);
 }
 
 //
@@ -74,31 +181,34 @@ function exerciseThree(books) {
 let people = [
   { name: "Chris", job: "Teacher" },
   { name: "Joanna", job: "Student" },
-  { name: "Boris", job: "Prime Minister" }
+  { name: "Boris", job: "Prime Minister" },
 ];
 
-exerciseOne(people);
+// exerciseOne(people);
 
 let shopping = ["Milk", "Break", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
 
-exerciseTwo(shopping);
+// exerciseTwo(shopping);
 
 const books = [
   {
     title: "The Design of Everyday Things",
     author: "Don Norman",
-    alreadyRead: false
+    alreadyRead: false,
+    url: "img/design.jpeg",
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
-    alreadyRead: true
+    alreadyRead: true,
+    url: "img/human.jpeg",
   },
   {
     title: "The Pragmatic Programmer",
     author: "Andrew Hunt",
-    alreadyRead: true
-  }
+    alreadyRead: true,
+    url: "img/programmer.jpeg",
+  },
 ];
 
 exerciseThree(books);
---
+++ week-2/Homework/2-project/js/main.js
@@ -0,0 +1,117 @@
+// - When clicking **blue** it should change:
+
+//   - **Jumbotron** background color to `#588fbd`
+//   - **Donate a bike** button background color to `#ffa500`
+//   - **Volunteer** button background color to `black` and text color to `white`
+
+document.getElementById("blueBtn").addEventListener("click", () => {
+  document.querySelector(".jumbotron").style.backgroundColor = "#588fbd";
+  document.querySelector(".jumbotron").style.color = "#ffffff";
+
+  document.querySelector(".jumbotron .btn-primary").style.backgroundColor =
+    "#ffa500";
+
+  document.querySelector(".jumbotron .btn-secondary").style.backgroundColor =
+    "#161616";
+  document.querySelector(".jumbotron .btn-secondary").style.color = "#ffffff";
+});
+
+// - When clicking **orange** it should change:
+
+//   - **Jumbotron** background color to `#f0ad4e`
+//   - **Donate a bike** button background color to `#5751fd`
+//   - **Volunteer** button background color to `#31b0d5` and text color to `white`
+
+document.getElementById("orangeBtn").addEventListener("click", () => {
+  document.querySelector(".jumbotron").style.backgroundColor = "#f0ad4e";
+  document.querySelector(".jumbotron").style.color = "#ffffff";
+
+  document.querySelector(".jumbotron .btn-primary").style.backgroundColor =
+    "#5751fd";
+
+  document.querySelector(".jumbotron .btn-secondary").style.backgroundColor =
+    "#31b0d5";
+  document.querySelector(".jumbotron .btn-secondary").style.color = "#ffffff";
+});
+
+// - When clicking **green** it should change:
+
+//   - **Jumbotron** background color to `#87ca8a`
+//   - **Donate a bike** button background color to `black`
+//   - **Volunteer** button background color to `#8c9c08`
+
+document.getElementById("greenBtn").addEventListener("click", () => {
+  document.querySelector(".jumbotron").style.backgroundColor = "#87ca8a";
+  document.querySelector(".jumbotron").style.color = "#ffffff";
+
+  document.querySelector(".jumbotron .btn-primary").style.backgroundColor =
+    "#161616";
+
+  document.querySelector(".jumbotron .btn-secondary").style.backgroundColor =
+    "#8c9c08";
+  document.querySelector(".jumbotron .btn-secondary").style.color = "#ffffff";
+});
+
+// ## Part 2
+
+// When the submit button is pressed, it should check that all the form fields are valid:
+// - The **Your name**, **Email** and **Describe yourself** fields need to be non-empty (Hint: their `value` length has to be greater than zero)
+// - For the **Email** field also check if it contains the `@` character
+// - For all the fields that invalid, it should make their background color `red`.
+
+// IF all the fields are valid, when you click **Submit** it should:
+// - Display an alert to thank you for filling out the form
+// - Blank out (make empty) all the text fields
+
+document.querySelector("form button").addEventListener("click", () => {
+  event.preventDefault();
+  let inputs = document.querySelectorAll("form .form-group");
+  checkingForms(inputs);
+});
+
+function checkingForms(inputs) {
+  let ok = "✔";
+  let empty = "*";
+  inputs.forEach((obj) => {
+    switch (obj.childNodes[3].type) {
+      case "email":
+        if (
+          obj.childNodes[3].value.length > 0 &&
+          obj.childNodes[3].value.split("").includes("@")
+        ) {
+          if (obj.childNodes[1].innerHTML.split("").includes(ok) === false) {
+            obj.childNodes[1].innerHTML += ` ${ok}`;
+          }
+        } else {
+          if (obj.childNodes[1].innerHTML.split("").includes(empty) === false) {
+            obj.childNodes[1].innerHTML += ` ${empty}`;
+          }
+        }
+        break;
+      case "text":
+        if (obj.childNodes[3].value.length > 0) {
+          if (obj.childNodes[1].innerHTML.split("").includes(ok) === false) {
+            obj.childNodes[1].innerHTML += ` ${ok}`;
+          }
+        } else {
+          if (obj.childNodes[1].innerHTML.split("").includes(empty) === false) {
+            obj.childNodes[1].innerHTML += ` ${empty}`;
+          }
+        }
+        break;
+      case "textarea":
+        if (obj.childNodes[3].value.length > 0) {
+          if (obj.childNodes[1].innerHTML.split("").includes(ok) === false) {
+            obj.childNodes[1].innerHTML += ` ${ok}`;
+          }
+        } else {
+          if (obj.childNodes[1].innerHTML.split("").includes(empty) === false) {
+            obj.childNodes[1].innerHTML += ` ${empty}`;
+          }
+        }
+    }
+  });
+}
+
+// **Important hint:** In your function that handles clicks on the `Submit` button you will need to call `event.preventDefault()`
+// to stop the browser from refreshing the page. To read more on how to do this: https://developer.mozilla.org/en/docs/Web/API/Event/preventDefault
