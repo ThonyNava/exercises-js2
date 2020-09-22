3b1dde5804df7c8d8c24f9e2cccca4258ada9b2b
Thony Nava <thonynava@Thonys-MacBook-Pro.local>
2020-09-22 18:46:22 +0200

    Js2 Wee-3 Homework

--- week-3/Homework/1-alarmclock/alarmclock.js
+++ week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,4 +1,102 @@
-function setAlarm() {}
+function setAlarm() {
+  let timeInput = document.getElementById("alarmSet").value;
+  let min = Math.floor(timeInput / 60);
+  let sec = Math.floor(timeInput - min * 60);
+  let timeRemaining = timeInput;
+
+  let counterIntervals = setInterval(() => {
+    if (timeInput > 0) {
+      timer(timeInput);
+    }
+  }, 1000);
+
+  let timer = (time) => {
+    if (timeRemaining > 0) {
+      document.body.style.backgroundColor = "rgba(0,255,0,.1)";
+
+      let minP = min;
+      let secP = sec;
+
+      if (min < 10) {
+        minP = `0${min}`;
+      }
+      if (sec < 10) {
+        secP = `0${sec}`;
+      }
+
+      let message = `Time Remaining: ${minP}:${secP}`;
+      document.getElementById("timeRemaining").innerHTML = message;
+
+      if (sec > 0) {
+        sec--;
+      } else {
+        min--;
+        sec = 59;
+      }
+
+      timeRemaining--;
+    } else {
+      playAlarm();
+      document.body.style.backgroundColor = "rgba(255,0,0,.1)";
+      document.getElementById("timeRemaining").innerHTML =
+        "Time Remaining: 00:00";
+      clearInterval(counterIntervals);
+    }
+
+    let cleaningButtons = () => {
+      document.querySelectorAll(".addedElement").forEach((obj) => {
+        obj.remove();
+      });
+    };
+
+    let restartingAll = () => {
+      document.body.style.backgroundColor = "white";
+      document.getElementById("timeRemaining").innerHTML =
+        "Time Remaining: 00:00";
+      document.getElementById("alarmSet").value = "";
+      document.getElementById("set").style.display = "inline-block";
+      clearInterval(counterIntervals);
+      cleaningButtons();
+      pauseAlarm();
+    };
+
+    cleaningButtons();
+    document.getElementById("set").style.display = "none";
+
+    let pauseButton = document.createElement("button");
+    pauseButton.id = "pause";
+    pauseButton.type = "button";
+    pauseButton.className = "addedElement btn btn-primary";
+    pauseButton.innerHTML = "Pause";
+
+    let buttonBox = document.getElementById("set").parentElement;
+    buttonBox.appendChild(pauseButton);
+
+    document.getElementById("pause").addEventListener("click", () => {
+      clearInterval(counterIntervals);
+      cleaningButtons();
+
+      let resumeButton = document.createElement("button");
+      resumeButton.id = "resume";
+      resumeButton.type = "button";
+      resumeButton.className = "addedElement btn btn-primary";
+      resumeButton.innerHTML = "Resume";
+      buttonBox.appendChild(resumeButton);
+
+      document.getElementById("resume").addEventListener("click", () => {
+        cleaningButtons();
+        counterIntervals = setInterval(() => {
+          timer(timeRemaining);
+        }, 1000);
+        buttonBox.appendChild(pauseButton);
+      });
+    });
+
+    document.getElementById("stop").addEventListener("click", () => {
+      restartingAll();
+    });
+  };
+}
 
 // DO NOT EDIT BELOW HERE
 
--- week-3/Homework/2-slideshow/slideshow.js
+++ week-3/Homework/2-slideshow/slideshow.js
@@ -1,1 +1,86 @@
 // Write your code here
+let slidexBox = document.getElementById("slidexBox");
+let btn_back = document.getElementById("back");
+let btn_forward = document.getElementById("forward");
+let div_1 = document.getElementById("img_1");
+let div_2 = document.getElementById("img_2");
+let div_3 = document.getElementById("img_3");
+let speed = 6;
+let pxInt = 10;
+let margin = -500;
+let url_1 = 'url("img/1.jpg")';
+let url_2 = 'url("img/2.jpg")';
+let url_3 = 'url("img/3.jpg")';
+let urls = [url_1, url_2, url_3];
+
+let backFnt = () => {
+  let moving = setInterval(() => {
+    if (margin < 0) {
+      margin += pxInt;
+      let mrgPx = `${margin}px`;
+      slidexBox.style.marginLeft = mrgPx;
+    } else {
+      urls.unshift(urls[2]);
+      urls.pop();
+
+      div_1.style.backgroundImage = urls[0];
+      div_2.style.backgroundImage = urls[1];
+      div_3.style.backgroundImage = urls[2];
+      clearInterval(moving);
+      margin = -500;
+      let mrgPx = `${margin}px`;
+      slidexBox.style.marginLeft = mrgPx;
+      state = false;
+    }
+  }, speed);
+};
+
+let forwardFnt = () => {
+  let moving = setInterval(() => {
+    if (margin > -1000) {
+      margin -= pxInt;
+      let mrgPx = `${margin}px`;
+      slidexBox.style.marginLeft = mrgPx;
+    } else {
+      urls.push(urls[0]);
+      urls.shift();
+
+      div_1.style.backgroundImage = urls[0];
+      div_2.style.backgroundImage = urls[1];
+      div_3.style.backgroundImage = urls[2];
+      clearInterval(moving);
+      margin = -500;
+      let mrgPx = `${margin}px`;
+      slidexBox.style.marginLeft = mrgPx;
+      state = false;
+    }
+  }, speed);
+};
+
+document.getElementById("back").addEventListener("click", () => {
+  backFnt();
+});
+
+document.getElementById("forward").addEventListener("click", () => {
+  forwardFnt();
+});
+
+document.getElementById("autoBack").addEventListener("click", () => {
+  backFnt();
+  let moving = setInterval(() => {
+    backFnt();
+  }, 3000);
+  document.getElementById("stop").addEventListener("click", () => {
+    clearInterval(moving);
+  });
+});
+
+document.getElementById("autoForward").addEventListener("click", () => {
+  forwardFnt();
+  let moving = setInterval(() => {
+    forwardFnt();
+  }, 3000);
+  document.getElementById("stop").addEventListener("click", () => {
+    clearInterval(moving);
+  });
+});
